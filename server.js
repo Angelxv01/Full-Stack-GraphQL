89de52eb1c13345cafbe733d45e4cfc981c5f34e
Angel <angeloxv@outlook.it>
2021-08-07 21:33:36 +0200

    part8: 8.7

--- server.js
+++ server.js
@@ -119,6 +119,7 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -148,6 +149,15 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const exist = authors.find((obj) => obj.name === args.name)
+      if (!exist) {
+        return
+      }
+      const author = { ...exist, born: args.setBornTo }
+      authors = authors.map((obj) => (obj.name === args.name ? author : obj))
+      return author
     }
   }
 }

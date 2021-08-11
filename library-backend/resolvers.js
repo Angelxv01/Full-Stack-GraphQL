c9515d96cdcd156f393073a9ff664a9995a1daf8
Angel <angeloxv@outlook.it>
2021-08-11 10:02:22 +0200

    part8: refactor code to use apollo-server-express

---
+++ library-backend/resolvers.js
@@ -0,0 +1,92 @@
+const { PubSub } = require('graphql-subscriptions')
+const Author = require('./models/author')
+const Book = require('./models/book')
+const User = require('./models/user')
+
+const pubSub = new PubSub()
+
+const resolvers = {
+  Query: {
+    authorsCount: () => Author.collection.countDocuments(),
+    booksCount: () => Book.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const books = args.genres
+        ? Book.find({ genres: { $in: [args.genres] } }).populate('author')
+        : Book.find({}).populate('author')
+      return books
+    },
+    allAuthors: () => Author.find({}),
+    me: (root, args, context) => context.currentUser
+  },
+  Author: {
+    bookCount: async (root) => {
+      const author = await Author.findOne({ name: root.name })
+      const res = await Book.countDocuments({ author: author._id })
+      return res
+    }
+  },
+  Mutation: {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new AuthenticationError('not authenticated')
+      }
+      const authorData = { name: args.author }
+      let author = await Author.findOne(authorData)
+      if (!author) {
+        author = new Author(authorData)
+        try {
+          await author.save()
+        } catch (err) {
+          throw new UserInputError(err.message, { invalidArgs: args })
+        }
+      }
+
+      const book = new Book({ ...args, author: author._id })
+      try {
+        await book.save()
+      } catch (err) {
+        throw new UserInputError(err.message, { invalidArgs: args })
+      }
+
+      await book.populate('author').execPopulate()
+      pubSub.publish('BOOK_ADDED', { bookAdded: book })
+      return book
+    },
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new AuthenticationError('not authenticated')
+      }
+      const author = await Author.findOneAndUpdate(
+        { name: args.name },
+        { $set: { born: args.setBornTo } },
+        { new: true }
+      )
+      return author
+    },
+    createUser: (root, args) => {
+      const user = new User({ ...args })
+      return user.save().catch((err) => {
+        throw new UserInputError(err.message, { invalidArgs: args })
+      })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+      if (!user || args.password !== 'password') {
+        throw new UserInputError('Invalid credentials')
+      }
+      const userToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userToken, process.env.SECRET) }
+    }
+  },
+  Subscription: {
+    bookAdded: {
+      subscribe: () => pubSub.asyncIterator(['BOOK_ADDED'])
+    }
+  }
+}
+
+module.exports = resolvers
--- library-backend/server.js
+++ library-backend/server.js
@@ -1,12 +1,20 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server-express')
+const express = require('express')
+const cors = require('cors')
+const { createServer } = require('http')
+const { execute, subscribe } = require('graphql')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const mongoose = require('mongoose')
 
-const Author = require('./models/author')
-const Book = require('./models/book')
 const User = require('./models/user')
+
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -22,134 +30,10 @@ mongoose
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const typeDefs = gql`
-  type Author {
-    name: String!
-    id: String!
-    born: Int
-    bookCount: Int!
-  }
-
-  type Book {
-    title: String!
-    published: Int!
-    author: Author!
-    id: ID!
-    genres: [String!]!
-  }
-
-  type User {
-    username: String!
-    favoriteGenre: String!
-    id: ID!
-  }
-
-  type Token {
-    value: String!
-  }
-
-  type Query {
-    booksCount: Int!
-    authorsCount: Int!
-    allBooks(author: String, genres: String): [Book!]!
-    allAuthors: [Author!]!
-    me: User
-  }
-
-  type Mutation {
-    addBook(
-      title: String!
-      published: Int!
-      author: String!
-      genres: [String!]!
-    ): Book
-    editAuthor(name: String!, setBornTo: Int!): Author
-    createUser(username: String!, favoriteGenre: String!): User
-    login(username: String!, password: String!): Token
-  }
-`
-
-const resolvers = {
-  Query: {
-    authorsCount: () => Author.collection.countDocuments(),
-    booksCount: () => Book.collection.countDocuments(),
-    allBooks: async (root, args) => {
-      const books = args.genres
-        ? Book.find({ genres: { $in: [args.genres] } }).populate('author')
-        : Book.find({}).populate('author')
-      return books
-    },
-    allAuthors: () => Author.find({}),
-    me: (root, args, context) => context.currentUser
-  },
-  Author: {
-    bookCount: async (root) => {
-      const author = await Author.findOne({ name: root.name })
-      const res = await Book.countDocuments({ author: author._id })
-      return res
-    }
-  },
-  Mutation: {
-    addBook: async (root, args, { currentUser }) => {
-      if (!currentUser) {
-        throw new AuthenticationError('not authenticated')
-      }
-      const authorData = { name: args.author }
-      let author = await Author.findOne(authorData)
-      if (!author) {
-        author = new Author(authorData)
-        try {
-          await author.save()
-        } catch (err) {
-          throw new UserInputError(err.message, { invalidArgs: args })
-        }
-      }
-
-      const book = new Book({ ...args, author: author._id })
-      try {
-        await book.save()
-      } catch (err) {
-        throw new UserInputError(err.message, { invalidArgs: args })
-      }
-
-      await book.populate('author').execPopulate()
-      return book
-    },
-    editAuthor: async (root, args, { currentUser }) => {
-      if (!currentUser) {
-        throw new AuthenticationError('not authenticated')
-      }
-      const author = await Author.findOneAndUpdate(
-        { name: args.name },
-        { $set: { born: args.setBornTo } },
-        { new: true }
-      )
-      return author
-    },
-    createUser: (root, args) => {
-      const user = new User({ ...args })
-      return user.save().catch((err) => {
-        throw new UserInputError(err.message, { invalidArgs: args })
-      })
-    },
-    login: async (root, args) => {
-      const user = await User.findOne({ username: args.username })
-      if (!user || args.password !== 'password') {
-        throw new UserInputError('Invalid credentials')
-      }
-      const userToken = {
-        username: user.username,
-        id: user._id
-      }
-
-      return { value: jwt.sign(userToken, process.env.SECRET) }
-    }
-  }
-}
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   context: async ({ req }) => {
     const auth = req ? req.headers.authorization : null
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
@@ -160,6 +44,27 @@ const server = new ApolloServer({
   }
 })
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
+server.start().then(() => {
+  const app = express()
+  app.use(cors())
+  server.applyMiddleware({ app })
+
+  const httpServer = createServer(app)
+
+  const subscriptionServer = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe
+    },
+    { server: httpServer, path: server.graphqlPath }
+  )
+
+  const signals = ['SIGINT', 'SIGTERM']
+
+  signals.forEach((signal) => {
+    process.on(signal, () => subscriptionServer.close())
+  })
+
+  httpServer.listen(4000, () => console.log('Listening on port 4000'))
 })
